6c1c927ec969d2fcbece809568bbea089f0f0e53
Denis Dolbilin <kombogame6@gmail.com>
2022-07-07 22:56:25 +0300

    feat: added filter buttons tab, added active filter state

--- src/pages/todo-page/todo-page.tsx
+++ src/pages/todo-page/todo-page.tsx
@@ -4,11 +4,15 @@ import styles from './todo-page.module.scss';
 import { TODO_INPUT_PLACEHOLDER } from './constants';
 import { TodoItem } from '../../components';
 import { observer } from 'mobx-react-lite';
+import { TodoFilterTab } from '../../components/todo-filter-tab';
+import { TFilterButtons } from '../../shared/types';
 
 export const TodoPage = observer(() => {
   const [createTodoInputValue, setCreateTodoInputValue] = useState('');
+  const [activeFilter, setActiveFilter] = useState<TFilterButtons>('Все');
   const addTodoClickhandler = () => {
     TodoStore.createTodo(createTodoInputValue);
+    setCreateTodoInputValue('');
   };
   return (
     <div className={styles.todo_wrapper}>
@@ -19,9 +23,18 @@ export const TodoPage = observer(() => {
           value={createTodoInputValue}
           onChange={e => setCreateTodoInputValue(e.target.value)}
         />
-        <button className={styles.create_todo_button} onClick={addTodoClickhandler}>Добавить</button>
+        <button
+          className={styles.create_todo_button}
+          onClick={addTodoClickhandler}
+        >
+          Добавить
+        </button>
       </div>
       <div className={styles.todo_container}>
+        <TodoFilterTab
+          filterButtonClickhandler={setActiveFilter}
+          activeFilter={activeFilter}
+        />
         {TodoStore.todoArr.map(item => (
           <TodoItem todoItem={item} key={item.id} />
         ))}
