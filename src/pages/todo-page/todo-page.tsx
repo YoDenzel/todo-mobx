b17b4b8fe6b1f2939d5de62cf3d6929d4a2e2469
Denis Dolbilin <kombogame6@gmail.com>
2022-07-07 14:03:01 +0300

    feat: added basic styles

--- src/pages/todo-page/todo-page.tsx
+++ src/pages/todo-page/todo-page.tsx
@@ -2,11 +2,18 @@ import styles from './todo-page.module.scss';
 
 export function TodoPage() {
   return (
-    <div className={styles.container}>
+    <div className={styles.todo_wrapper}>
       <input
         className={styles.create_todo_input}
         placeholder="Введите действие"
       />
+      <div className={styles.todo_container}>
+        {[1, 2, 3, 4, 5].map(item => (
+          <div className={styles.todo_card} key={item}>
+            <p className={styles.todo_card__title}>{item}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
