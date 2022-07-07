02c98a52e9c7233990fe01bfed0b9a458e46c084
Denis Dolbilin <kombogame6@gmail.com>
2022-07-07 21:34:50 +0300

    feat: logic for delete, create and check/unchek todo

--- src/store/todo-store.ts
+++ src/store/todo-store.ts
@@ -1,22 +1,37 @@
-import { makeAutoObservable } from 'mobx';
-import { observer } from 'mobx-react-lite';
+import { action, makeObservable, observable } from 'mobx';
+import { TTodoItem } from '../shared/types';
 
-type TTodoItem = {
-  id: number;
-  name: string;
-  isDone: boolean;
-};
-
-class TodoStore {
+export class TodoStoreImplementation {
   todoArr = [] as TTodoItem[];
 
-  createTodo(item: TTodoItem) {
-    this.todoArr.push(item);
+  createTodo(name: string) {
+    const todoItem = {
+      id: Date.now(),
+      name: name,
+      isDone: false,
+    };
+    this.todoArr.push(todoItem);
+  }
+  setIsDone(todo: TTodoItem) {
+    this.todoArr = this.todoArr.filter(item => {
+      if (todo.id === item.id) {
+        item.isDone = !item.isDone;
+        return item;
+      } else return item;
+    });
+  }
+  deleteTodo(todo: TTodoItem) {
+    this.todoArr = this.todoArr.filter(item => !(item.id === todo.id));
   }
 
   constructor() {
-    makeAutoObservable(this);
+    makeObservable(this, {
+      todoArr: observable,
+      createTodo: action,
+      setIsDone: action,
+      deleteTodo: action,
+    });
   }
 }
 
-export default new TodoStore();
+export const TodoStore = new TodoStoreImplementation();
