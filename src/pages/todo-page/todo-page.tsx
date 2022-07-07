9793bfa2609c68f41336f3f571402d339e1280fd
Denis Dolbilin <kombogame6@gmail.com>
2022-07-07 21:36:59 +0300

    feat: styles, creating todo e.tc

--- src/pages/todo-page/todo-page.tsx
+++ src/pages/todo-page/todo-page.tsx
@@ -1,19 +1,31 @@
+import { useState } from 'react';
+import { TodoStore } from '../../store/todo-store';
 import styles from './todo-page.module.scss';
+import { TODO_INPUT_PLACEHOLDER } from './constants';
+import { TodoItem } from '../../components';
+import { observer } from 'mobx-react-lite';
 
-export function TodoPage() {
+export const TodoPage = observer(() => {
+  const [createTodoInputValue, setCreateTodoInputValue] = useState('');
+  const addTodoClickhandler = () => {
+    TodoStore.createTodo(createTodoInputValue);
+  };
   return (
     <div className={styles.todo_wrapper}>
-      <input
-        className={styles.create_todo_input}
-        placeholder="Введите действие"
-      />
+      <div className={styles.create_todo_container}>
+        <input
+          className={styles.create_todo_input}
+          placeholder={TODO_INPUT_PLACEHOLDER}
+          value={createTodoInputValue}
+          onChange={e => setCreateTodoInputValue(e.target.value)}
+        />
+        <button className={styles.create_todo_button} onClick={addTodoClickhandler}>Добавить</button>
+      </div>
       <div className={styles.todo_container}>
-        {[1, 2, 3, 4, 5].map(item => (
-          <div className={styles.todo_card} key={item}>
-            <p className={styles.todo_card__title}>{item}</p>
-          </div>
+        {TodoStore.todoArr.map(item => (
+          <TodoItem todoItem={item} key={item.id} />
         ))}
       </div>
     </div>
   );
-}
+});
