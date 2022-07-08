b543c53ce8ecdf7ac444611b23a03120d00b6c53
Denis Dolbilin <kombogame6@gmail.com>
2022-07-08 17:11:33 +0300

    feat: added filtering

--- src/pages/todo-page/todo-page.tsx
+++ src/pages/todo-page/todo-page.tsx
@@ -11,36 +11,39 @@ import { TFilterButtons } from '../../shared/types';
 export const TodoPage = observer(() => {
   const [createTodoInputValue, setCreateTodoInputValue] = useState('');
   const [activeFilter, setActiveFilter] = useState<TFilterButtons>('Все');
-  const addTodoClickhandler = () => {
-    TodoStore.createTodo(createTodoInputValue);
-    setCreateTodoInputValue('');
+  const addTodoClickhandler = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (createTodoInputValue) {
+      TodoStore.createTodo(createTodoInputValue);
+      setCreateTodoInputValue('');
+    }
   };
   const todos = computed(() => TodoStore.filterTodo(activeFilter)).get();
 
   const filterButtonClickhandler = (v: TFilterButtons) => {
     setActiveFilter(v);
   };
-  console.log(todos);
   return (
     <div className={styles.todo_wrapper}>
-      <div className={styles.create_todo_container}>
+      <form
+        className={styles.create_todo_container}
+        onSubmit={e => addTodoClickhandler(e)}
+      >
         <input
           className={styles.create_todo_input}
           placeholder={TODO_INPUT_PLACEHOLDER}
           value={createTodoInputValue}
           onChange={e => setCreateTodoInputValue(e.target.value)}
         />
-        <button
-          className={styles.create_todo_button}
-          onClick={addTodoClickhandler}
-        >
+        <button className={styles.create_todo_button} type="submit">
           Добавить
         </button>
-      </div>
+      </form>
       <div className={styles.todo_container}>
         <TodoFilterTab
           filterButtonClickhandler={filterButtonClickhandler}
           activeFilter={activeFilter}
+          isTodos={!!TodoStore.todoArr.length}
         />
         {todos.map(item => (
           <TodoItem todoItem={item} key={item.id} />
